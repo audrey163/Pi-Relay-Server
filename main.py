69ae005d3a3b1b90fe846b4af2c6e0914ed74725
Audrey Palmore <audreypalmore1@gmail.com>
2022-04-09 19:25:34 -0500

    created server

---
+++ main.py
@@ -0,0 +1,28 @@
+from relay import Relay
+from flask import Flask, request
+
+app = Flask(__name__) #init flask app
+r = Relay() #init GPIO-Relay handler
+
+@app.route('/')
+def index(): #this is good just to make sure the server is up
+    return 'ready'
+
+@app.route('/on', methods = ['GET'])
+def on():
+    ch = request.args.get('ch')
+    if ch in [ str(i) for i in range(1,9) ]:
+        r.on(ch)
+        return 'success'
+    return 'error'
+
+@app.route('/off', methods = ['GET'])
+def off():
+    ch = request.args.get('ch')
+    if ch in [ str(i) for i in range(1,9) ]:
+        r.off(ch)
+        return 'success'
+    return 'error'
+
+if __name__ == '__main__':
+    app.run(debug=False, port=420, host='0.0.0.0')
---
+++ relay.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+
+class Relay():
+    def __init__(self):
+        self.channels = {'1' : 26,'2' : 19, '3' : 13, '4' : 6, '5' : 12, '6' : 16, '7' : 20,'8' : 21}
+
+        GPIO.setmode(GPIO.BCM)
+        for ch in list(self.channels):
+            GPIO.setup(self.channels[ch], GPIO.OUT)
+            GPIO.output(self.channels[ch], GPIO.HIGH)
+
+    def __del__(self):
+        GPIO.cleanup()
+
+    def off(self,ch):
+        GPIO.output(self.channels[ch], GPIO.LOW)
+
+    def on(self,ch):
+        GPIO.output(self.channels[ch], GPIO.HIGH)
